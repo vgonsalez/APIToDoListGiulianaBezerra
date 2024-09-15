96e538ad467d79072ecf8a91dd4b60fb03989aa5
vgonsalez <gonsalez@yahoo.com>
2024-09-15 13:03:47 -0300

    geração da entidade ToDo

--- src/main/java/br/com/apitodolistgiulianabezerra/todolist/entity/Todo.java
+++ src/main/java/br/com/apitodolistgiulianabezerra/todolist/entity/Todo.java
@@ -1,5 +1,15 @@
 package br.com.apitodolistgiulianabezerra.todolist.entity;
 
-public class Todo {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="todos")
+public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
